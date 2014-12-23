3365a02292d7687dff591b26041fc8a6ded0de0b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-12-23 05:55:09 +0000

    Initial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='test-repo',
+    version='0.1.0',
+    description='Just test project.',
+    author='Alexander Artemenko',
+    author_email='svetlyak.40wt@gmail.com',
+)
